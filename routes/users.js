98eebb89247d8cc4b2079be04f2c389040bd853b
scumbagblues <rcr.786@gmail.com>
2017-05-14 03:03:23 -0500

    Finished crud in express and nodejs

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,6 @@ router.get('/add', function(req, res, next) {
 });
 
 router.post("/add", function (req, res, next) {
-    //console.log(User.fetchAll());
     new User({
         name: req.body.name,
         email: req.body.email,
@@ -20,7 +19,43 @@ router.post("/add", function (req, res, next) {
         .catch(function (error) {
             res.end(error.message);
         });
+});
 
+router.get("/update/:id",function(req,res,next){
+
+    User.where({id:req.params.id}).fetch()
+        .then(function(user){
+            res.render("users/update",{item:user.toJSON(),contextPath:""})
+
+        })
+        .catch(function(error){
+            res.end(error.message);
+        });
+});
+
+router.post("/update/:id",function(req,res,next){
+    req.body.id=req.params.id;
+
+    new User()
+        .save(req.body,{patch:true})
+        .then(function(result){
+            res.redirect("/");
+
+        }).catch(function(error){
+        res.end(error.message);
+    });
+
+});
+
+router.get("/delete/:id",function(req,res,next){
+    new User({id: req.params.id})
+        .destroy()
+        .then(function(model){
+            res.redirect("/");
+        })
+        .catch(function(error){
+            res.end(error.message);
+        });
 
 });
 
